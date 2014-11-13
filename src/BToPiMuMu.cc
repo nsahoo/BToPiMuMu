47477582cd8a1c93feaed230016f08fb9fbd9518
nsahoo <niladribihari.sahoo@cern.ch>
2014-11-13 16:02:05 +0100

    minor update

--- src/BToPiMuMu.cc
+++ src/BToPiMuMu.cc
@@ -32,7 +32,7 @@
 #include "FWCore/Common/interface/TriggerNames.h"
 
 #include "MagneticField/Engine/interface/MagneticField.h"
-
+//#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
 
 //#include "DataFormats/Common/interface/Handle.h"
 //#include "DataFormats/Common/interface/TriggerResults.h"
@@ -171,6 +171,9 @@ class BToPiMuMu : public edm::EDAnalyzer {
   bool hasBeamSpot(const edm::Event&);
   bool hasPrimaryVertex(const edm::Event &);
   void clearVariables();
+  void hltReport(const edm::Event&);
+  void saveGenInfo(const edm::Event&);
+  void saveTruthMatch(const edm::Event&);
 
 
 
@@ -403,9 +406,15 @@ BToPiMuMu::~BToPiMuMu()
 void
 BToPiMuMu::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-  //   using namespace edm;
 
+  n_processed_ += 1;
+  histos[h_events]->Fill(0);
 
+  clearVariables();
+
+  run = iEvent.id().run() ;
+  event = iEvent.id().event() ;
+  lumiblock = iEvent.luminosityBlock();
 
 
 }
@@ -415,12 +424,139 @@ BToPiMuMu::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 void 
 BToPiMuMu::beginJob()
 {
+
+
+  t_begin_.Set();
+  printf("\n ---------- Begin Job ---------- \n"); /* added */
+  t_begin_.Print();
+
+
+  n_processed_ = 0;
+  n_selected_ = 0;
+
+
+  fout_ = new TFile(OutputFileName_.c_str(), "RECREATE");
+  fout_->cd();
+
+
+  for(int i=0; i<kHistNameSize; i++) {
+    histos[i] = new TH1F(hist_args[i].name, hist_args[i].title,
+			 hist_args[i].n_bins,
+			 hist_args[i].x_min, hist_args[i].x_max);
+  }
+
+
+  tree_ = new TTree ("tree", "BToPiMuMu");
+
+  tree_->Branch("run", &run, "run/i");
+  tree_->Branch("event", &event, "event/i");
+  tree_->Branch("lumiblock", &lumiblock, "lumiblock/i");
+  tree_->Branch("nprivtx", &nprivtx, "nprivtx/i");
+  tree_->Branch("triggernames", &triggernames);
+  tree_->Branch("triggerprescales", &triggerprescales);
+  tree_->Branch("mumdcabs", &mumdcabs);
+  tree_->Branch("mumdcabserr", &mumdcabserr);
+  tree_->Branch("mumpx", &mumpx);
+  tree_->Branch("mumpy", &mumpy);
+  tree_->Branch("mumpz", &mumpz);
+  tree_->Branch("mupdcabs", &mupdcabs);
+  tree_->Branch("mupdcabserr", &mupdcabserr);
+  tree_->Branch("muppx", &muppx);
+  tree_->Branch("muppy", &muppy);
+  tree_->Branch("muppz", &muppz);
+  tree_->Branch("mumutrkr", &mumutrkr);
+  tree_->Branch("mumutrkz", &mumutrkz);
+  tree_->Branch("mumudca", &mumudca);
+  tree_->Branch("mumuvtxcl", &mumuvtxcl);
+  tree_->Branch("mumulsbs", &mumulsbs);
+  tree_->Branch("mumulsbserr", &mumulsbserr);
+  tree_->Branch("mumucosalphabs", &mumucosalphabs);
+  tree_->Branch("mumucosalphabserr", &mumucosalphabserr);
+  tree_->Branch("mumumass", &mumumass);
+  tree_->Branch("mumumasserr", &mumumasserr);
+  tree_->Branch("mumisgoodmuon", &mumisgoodmuon);
+  tree_->Branch("mupisgoodmuon", &mupisgoodmuon);
+  tree_->Branch("mumnpixhits", &mumnpixhits);
+  tree_->Branch("mupnpixhits", &mupnpixhits);
+  tree_->Branch("mumnpixlayers", &mumnpixlayers);
+  tree_->Branch("mupnpixlayers", &mupnpixlayers);
+  tree_->Branch("mumntrkhits", &mumntrkhits);
+  tree_->Branch("mupntrkhits", &mupntrkhits);
+  tree_->Branch("mumntrklayers", &mumntrklayers);
+  tree_->Branch("mupntrklayers", &mupntrklayers);
+  tree_->Branch("mumnormchi2", &mumnormchi2);
+  tree_->Branch("mupnormchi2", &mupnormchi2);
+  tree_->Branch("mumdxyvtx", &mumdxyvtx);
+  tree_->Branch("mupdxyvtx", &mupdxyvtx);
+  tree_->Branch("mumdzvtx", &mumdzvtx);
+  tree_->Branch("mupdzvtx", &mupdzvtx);
+  tree_->Branch("mumtriglastfilter", &mumtriglastfilter);
+  tree_->Branch("muptriglastfilter", &muptriglastfilter);
+  tree_->Branch("mumpt", &mumpt);
+  tree_->Branch("muppt", &muppt);
+  tree_->Branch("mumeta", &mumeta);
+  tree_->Branch("mupeta", &mupeta);
+  tree_->Branch("trkchg", &trkchg);
+  tree_->Branch("trkpx", &trkpx);
+  tree_->Branch("trkpy", &trkpy);
+  tree_->Branch("trkpz", &trkpz);
+  tree_->Branch("trkpt", &trkpt);
+  tree_->Branch("trkdcabs", &trkdcabs);
+  tree_->Branch("trkdcabserr", &trkdcabserr);
+
+  tree_->Branch("nb", &nb, "nb/I");
+  tree_->Branch("bchg", &bchg);
+  tree_->Branch("bpx", &bpx);
+  tree_->Branch("bpxerr", &bpxerr);
+  tree_->Branch("bpy", &bpy);
+  tree_->Branch("bpyerr", &bpyerr);
+  tree_->Branch("bpz", &bpz);
+  tree_->Branch("bpzerr", &bpzerr);
+  tree_->Branch("bmass", &bmass);
+  tree_->Branch("bmasserr", &bmasserr);
+  tree_->Branch("bvtxcl", &bvtxcl);
+  tree_->Branch("bvtxx", &bvtxx);
+  tree_->Branch("bvtxxerr", &bvtxxerr);
+  tree_->Branch("bvtxy", &bvtxy);
+  tree_->Branch("bvtxyerr", &bvtxyerr);
+  tree_->Branch("bvtxz", &bvtxz);
+  tree_->Branch("bvtxzerr", &bvtxzerr);
+  tree_->Branch("bcosalphabs", &bcosalphabs);
+  tree_->Branch("bcosalphabserr", &bcosalphabserr);
+
+  tree_->Branch("bcosalphabs2d", &bcosalphabs2d);
+  tree_->Branch("bcosalphabs2derr", &bcosalphabs2derr);
+  tree_->Branch("blsbs", &blsbs);
+  tree_->Branch("blsbserr", &blsbserr);
+  tree_->Branch("bctau", &bctau);
+  tree_->Branch("bctauerr", &bctauerr);
+
+
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
 void 
 BToPiMuMu::endJob() 
 {
+
+  fout_->cd();
+  tree_->Write();
+
+  for(int i = 0; i < kHistNameSize; i++) {
+    histos[i]->Write();
+    histos[i]->Delete();
+  }
+
+  fout_->Close();
+  t_now_.Set();
+  printf(" \n ---------- End Job ---------- \n" ) ; /* added */
+  t_now_.Print();
+  printf(" processed: %i \n selected: %i \n \
+duration: %i sec \n rate: %g evts/sec\n",
+	 n_processed_, n_selected_,
+	 t_now_.Convert() - t_begin_.Convert(),
+	 float(n_processed_)/(t_now_.Convert()-t_begin_.Convert()) );
+
 }
 
 // ------------ method called when starting to processes a run  ------------
@@ -455,6 +591,47 @@ BToPiMuMu::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
   desc.setUnknown();
   descriptions.addDefault(desc);
+}
+
+void 
+BToPiMuMu::clearVariables(){
+
+  run = 0;
+  event = 0;
+  lumiblock = 0;
+  nprivtx = 0;
+  triggernames->clear();
+  triggerprescales->clear();
+  mumdcabs->clear(); mumdcabserr->clear(); mumpx->clear(); mumpy->clear();
+  mumpz->clear();
+  mupdcabs->clear(); mupdcabserr->clear(); muppx->clear(); muppy->clear();
+  muppz->clear();
+  mumutrkr->clear(); mumutrkz->clear();
+  mumudca->clear(); mumuvtxcl->clear(); mumulsbs->clear(); mumulsbserr->clear();
+  mumucosalphabs->clear(); mumucosalphabserr->clear();
+  mumumass->clear(); mumumasserr->clear();
+  mumisgoodmuon->clear(); mupisgoodmuon->clear();
+  mumnpixhits->clear(); mupnpixhits->clear(); mumnpixlayers->clear();
+  mupnpixlayers->clear();
+  mumntrkhits->clear(); mupntrkhits->clear(); mumntrklayers->clear();
+  mupntrklayers->clear();
+  mumnormchi2->clear(); mupnormchi2->clear(); mumdxyvtx->clear(); mupdxyvtx->clear();
+  mumdzvtx->clear(); mupdzvtx->clear(); mumtriglastfilter->clear();
+  muptriglastfilter->clear();
+  mumpt->clear(); muppt->clear();
+  mumeta->clear(); mupeta->clear();
+  trkchg->clear(); trkpx->clear(); trkpy->clear(); trkpz->clear();
+  trkpt->clear();
+  trkdcabs->clear(); trkdcabserr->clear();
+  bpx->clear(); bpxerr->clear(); bpy->clear(); bpyerr->clear();
+  bpz->clear(); bpzerr->clear(); nb = 0;
+  bchg->clear(); bmass->clear(); bmasserr->clear();
+  bvtxcl->clear(); bvtxx->clear(); bvtxxerr->clear(); bvtxy->clear();
+  bvtxyerr->clear();
+  bvtxz->clear(); bvtxzerr->clear(); bcosalphabs->clear(); bcosalphabserr->clear();
+  blsbs->clear(); blsbserr->clear(); bctau->clear(); bctauerr->clear();
+  bcosalphabs2d->clear(); bcosalphabs2derr->clear();
+
 }
 
 
